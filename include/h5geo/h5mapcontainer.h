1ed3c3200461720cf92032758581ce45daad9589
kerim371 <kerim.khemraev@mail.ru>
2022-06-23 22:59:39 +0300

    Added doxygen docs for `h5mapcontainer.h`

--- include/h5geo/h5mapcontainer.h
+++ include/h5geo/h5mapcontainer.h
@@ -8,20 +8,26 @@
 
 class H5Map;
 
+/// \class H5MapContainer
+/// \brief Provides API to work with maps
 class H5MapContainer : public H5BaseContainer
 {
 protected:
   virtual ~H5MapContainer() = default;
 
 public:
+  /// \brief Open map
   virtual H5Map* openMap(
       const std::string& name) = 0;
+	/// \brief Open map
   virtual H5Map* openMap(
       h5gt::Group group) = 0;
+	/// \brief Create map
   virtual H5Map* createMap(
       std::string& name,
       MapParam &p,
       h5geo::CreationType createFlag) = 0;
+	/// \brief Create map
   virtual H5Map* createMap(
       h5gt::Group group,
       MapParam &p,
