4d40022cb629aad96c68b5027c47e18f98b4dfa4
kerim371 <kerim.khemraev@mail.ru>
2022-06-23 15:29:14 +0300

    Added doxygen docs for `H5BasePoints`

--- include/h5geo/h5basepoints.h
+++ include/h5geo/h5basepoints.h
@@ -5,21 +5,30 @@
 
 class H5BaseContainer;
 
+/// \class H5BasePoints
+/// \brief Base class for Points
 class H5BasePoints : public H5BaseObject
 {
 protected:
   virtual ~H5BasePoints() = default;
 
 public:
+  /// \brief Resize HDF5 DataSet
   virtual bool setNPoints(size_t n) = 0;
+  /// \brief Set domain for the points (time, depth etc)
   virtual bool setDomain(const h5geo::Domain& domain) = 0;
 
+  /// \brief Open container where current points resides
   virtual H5BaseContainer* openContainer() const = 0;
+  /// \brief Return number of points
   virtual size_t getNPoints() = 0;
+  /// \brief Get domain (time, depth etc)
   virtual h5geo::Domain getDomain() = 0;
 
+  /// \brief Get parameters that were used to create current points
   virtual PointsParam getParam() = 0;
 
+  /// \brief Get current points DataSet
   virtual std::optional<h5gt::DataSet> getPointsD() const = 0;
 };
 
