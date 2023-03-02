6335832ba83fdabef547809bcffb57139b0b9fe4
kerim371 <kerim371@users.noreply.github.com>
2023-03-02 08:55:35 +0000

    deploy: 7a6a09e7ca5e38ee327272fc96d66be378120060

--- search/all_4.js
+++ search/all_4.js
@@ -3,5 +3,8 @@ var searchData=
   ['datatype_0',['dataType',['../struct_seis_param.html#a8bbc13aabca2bcecb2f2c185cb6820c1',1,'SeisParam']]],
   ['dataunits_1',['dataUnits',['../struct_base_object_param.html#ababd8f266da92c28f2f1092a9664e83e',1,'BaseObjectParam']]],
   ['devcurveparam_2',['DevCurveParam',['../struct_dev_curve_param.html',1,'']]],
-  ['domain_3',['domain',['../struct_points_param.html#ad384f6e1e390c47df92eb17ccaa60f74',1,'PointsParam::domain()'],['../struct_horizon_param.html#a134cbb9fa6f33b2447231035ffd83dd0',1,'HorizonParam::domain()'],['../struct_map_param.html#acd371c594a064661ffce58ee4f616e75',1,'MapParam::domain()'],['../struct_vol_param.html#acfcf5ca9518feccb57218b664ee1aa4c',1,'VolParam::domain()'],['../struct_seis_param.html#a14aa09ef452c7c636b864ba6ecbf5a29',1,'SeisParam::domain()']]]
+  ['domain_3',['domain',['../struct_points_param.html#ad384f6e1e390c47df92eb17ccaa60f74',1,'PointsParam::domain()'],['../struct_horizon_param.html#a134cbb9fa6f33b2447231035ffd83dd0',1,'HorizonParam::domain()'],['../struct_map_param.html#acd371c594a064661ffce58ee4f616e75',1,'MapParam::domain()'],['../struct_vol_param.html#acfcf5ca9518feccb57218b664ee1aa4c',1,'VolParam::domain()'],['../struct_seis_param.html#a14aa09ef452c7c636b864ba6ecbf5a29',1,'SeisParam::domain()']]],
+  ['dx_4',['dX',['../struct_vol_param.html#a920ac48cfb1e2a2507a281eb439d3f9b',1,'VolParam']]],
+  ['dy_5',['dY',['../struct_vol_param.html#a6ef5a17682e42047183623c33d2eb5e3',1,'VolParam']]],
+  ['dz_6',['dZ',['../struct_vol_param.html#af310af4cb2d8297adc6894186e1756b2',1,'VolParam']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['chunksize_0',['chunkSize',['../struct_points_param.html#a078b151575a3af703a20875a29d2012e',1,'PointsParam::chunkSize()'],['../struct_dev_curve_param.html#a40a33ad07e837776c01400f49b8bd733',1,'DevCurveParam::chunkSize()'],['../struct_log_curve_param.html#a267273a228df66fe71da7742fe910f8c',1,'LogCurveParam::chunkSize()']]],
-  ['components_1',['components',['../struct_horizon_param.html#a245cacf450dc3ead04559a056a9488d6',1,'HorizonParam']]]
+  ['components_1',['components',['../struct_horizon_param.html#a245cacf450dc3ead04559a056a9488d6',1,'HorizonParam']]],
+  ['compression_5flevel_2',['compression_level',['../struct_vol_param.html#a057037ec873fc52a6c7991ff028f74c5',1,'VolParam']]]
 ];
