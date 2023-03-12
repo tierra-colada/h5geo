4bd1fda0d01c7534e606ac036b69316fe2c6c3b1
kerim371 <kerim371@users.noreply.github.com>
2023-03-12 22:35:27 +0000

    deploy: d6e7b9608fdeefe4688c8fa53f303e3d40369bf0

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['baseobjectparam_0',['BaseObjectParam',['../struct_base_object_param.html',1,'']]]
+  ['baseobjectparam_0',['BaseObjectParam',['../struct_base_object_param.html',1,'']]],
+  ['binheader_1',['BinHeader',['../structh5geo_1_1_bin_header.html',1,'h5geo']]]
 ];
